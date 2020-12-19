a4d39889ce2d2b83674fd49764f85915371d0d4c
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2020-12-19 18:16:53 +0300

    Add setup.py and GitHub actions for distribution

---
+++ setup.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+
+def read(relative: str) -> str:
+    base: Path = Path(__file__).parent
+    return base.joinpath(relative).read_text()
+
+
+setup(
+    name='detector',
+    version='0.1.0',
+    author='Tuna Alikasifoglu',
+    author_email='tunakasif@gmail.com',
+    packages=find_packages(),
+    install_requires=["cv2"],
+)
