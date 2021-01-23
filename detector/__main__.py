540bd1853004d4cb03b5559cabcf47d09efa212d
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2021-01-23 15:53:56 +0300

    FPS trial without display

--- detector/__main__.py
+++ detector/__main__.py
@@ -3,20 +3,21 @@ from detector import Detector
 import cv2
 import time
 
-with Detector(0, None) as detector:
+with Detector(1, None) as detector:
     target = None
     while True:
         start = time.time()
         frame, target, center_x, center_y = detector.detect(target)
-        if center_x and center_y:
-            cv2.putText(frame, f'Center: ({center_x:0.3f}, {center_y:0.3f})', (20, 80),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
-        else:
-            cv2.putText(frame, f'Center: None', (20, 80),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
+        # if center_x and center_y:
+        #     cv2.putText(frame, f'Center: ({center_x:0.3f}, {center_y:0.3f})', (20, 80),
+        #                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
+        # else:
+        #     cv2.putText(frame, f'Center: None', (20, 80),
+        #                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
         if cv2.waitKey(1) == 27:
             break
-        cv2.putText(frame, f'FPS: {1/(time.time() - start):0.0f}', (20, 120),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
-        cv2.imshow('Detected', frame)
+        # cv2.putText(frame, f'FPS: {1/(time.time() - start):0.0f}', (20, 120),
+        #             cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 2)
+        # cv2.imshow('Detected', frame)
+        print(f'FPS: {1/(time.time() - start):0.0f}')
     cv2.destroyAllWindows()
